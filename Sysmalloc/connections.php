b50e5b57a6e7c106e152bce2f9a0cb8dc5c245a6
Geeky-Noob <97742554+Geeky-Noob@users.noreply.github.com>
2023-04-01 15:17:51 +0530

    Add files via upload

---
+++ Sysmalloc/connections.php
@@ -0,0 +1,7 @@
+<?php
+$servername="localhost";
+$username="";
+$password="";
+$db_name="";
+$conn=new mysqli($servername, $username, $password, $db_name);
+?>
--- Sysmalloc/script.js
+++ Sysmalloc/script.js
@@ -1,11 +1,16 @@
-//   alert("You clicked the sign up link!");
-// Get the sign-up button or link
-var signupBtn = document.getElementById("signup-btn");
-// Get the pop-up box
-var popupBox = document.querySelector(".popup-box");
-// When the user clicks on the sign-up button or link, show the pop-up box
-signupBtn.addEventListener("click", function (event) {
-  event.preventDefault();
+function login_func() {
+  var popupBox = document.querySelector(".pop-outer1");
   popupBox.style.display = "block";
-});
-// When the user clicks on the close button or outside the pop-up box, hide the pop-up box
+}
+function sign_up_func() {
+  var popupBox = document.querySelector(".pop-outer2");
+  popupBox.style.display = "block";
+}
+function cancel1() {
+  var popupBox = document.querySelector(".pop-outer1");
+  popupBox.style.display = "none";
+}
+function cancel2() {
+  var popupBox = document.querySelector(".pop-outer2");
+  popupBox.style.display = "none";
+}
