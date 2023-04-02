20b9725a169e1910b099c3373fe4cc3bb37a15ff
Geeky-Noob <97742554+Geeky-Noob@users.noreply.github.com>
2023-04-02 15:37:05 +0530

    Add files via upload

--- Sysmalloc/script.js
+++ Sysmalloc/script.js
@@ -6,11 +6,3 @@ function sign_up_func() {
   var popupBox = document.querySelector(".pop-outer2");
   popupBox.style.display = "block";
 }
-function cancel1() {
-  var popupBox = document.querySelector(".pop-outer1");
-  popupBox.style.display = "none";
-}
-function cancel2() {
-  var popupBox = document.querySelector(".pop-outer2");
-  popupBox.style.display = "none";
-}
