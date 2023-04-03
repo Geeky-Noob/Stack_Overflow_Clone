3ddd097f99395495afce1d9227fd7753d840145d
Geeky-Noob <97742554+Geeky-Noob@users.noreply.github.com>
2023-04-03 13:13:23 +0530

    Add files via upload

--- Sysmalloc/script.js
+++ Sysmalloc/script.js
@@ -2,7 +2,35 @@ function login_func() {
   var popupBox = document.querySelector(".pop-outer1");
   popupBox.style.display = "block";
 }
+
+const username =
+  "username" ?? document.querySelector('input[name="name2"]').value;
+const email =
+  "user@gmail.com" ?? document.querySelector('input[name="email2"]').value;
+const password =
+  "pswrd1" ?? document.querySelector('input[name="password2"]').value;
+
+console.log(username, email, password);
+
 function sign_up_func() {
   var popupBox = document.querySelector(".pop-outer2");
   popupBox.style.display = "block";
+}
+
+function sign_up_call() {
+  console.log(12345);
+  fetch("http://10.54.18.58:5000/members", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((error) => console.error(error));
 }
